ffa6bf9fc7d1c25cb4fdec9e8aa1a9cbbbcff835
AbiskarLc <lcabi116@gmail.com>
2022-12-16 19:05:49 +0545

    added c files

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+	int a,b,add=0;
+	printf("\n Enter the value of a and b:");
+	scanf("%d%d",&a,&b);
+	add=a+b;
+	printf("\n The addition of a and b is:%d",add);
+	
+}
